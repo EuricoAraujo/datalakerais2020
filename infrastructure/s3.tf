79a263b805bbc6b84d6ac33d1cb5b0a3ff5d330f
Eurico Araujo <eurico.oneiros@gmail.com>
2022-03-19 19:45:54 -0300

    retirada da função server_side_encryption_configuration

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -5,11 +5,4 @@ resource "aws_s3_bucket" "datalake" {
     CURSO = "EDC"
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
