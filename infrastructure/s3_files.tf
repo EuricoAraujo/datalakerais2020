a479f5616a8bd45031f9cec412d7c8186dce0583
Eurico Araujo <eurico.oneiros@gmail.com>
2022-03-19 20:25:05 -0300

    correção da variavel bucket no resource

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,12 +1,12 @@
 resource "aws_s3_bucket_object" "etl" {
-  bucket = aws_s3_bucket.datalake-eurico-atv1.id
+  bucket = aws_s3_bucket.datalake.id
   key = "emr-code/pyspark/etl.py"
   source = "../etl/etl.py"
   etag = filemd5("../etl/etl.py")
 }
 
 resource "aws_s3_bucket_object" "lambda" {
-  bucket = aws_s3_bucket.datalake-eurico-atv1.id
+  bucket = aws_s3_bucket.datalake.id
   key = "emr-code/lambdafunctions/lambda_function.py"
   source ="../etl/lambda_function.py"
   etag = filemd5("../etl/lambda_function.py")
